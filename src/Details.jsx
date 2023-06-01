460a4b5b00aa93d9b654464d8279af763c54138a
oluwakemi <yusufoluwakemi39@gmail.com>
2023-06-01 19:56:27 +0100

    implemented modal in react using portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPets from "./fetchPets";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const Details = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false)
   const { isLoading, data } = useQuery(["details", id], fetchPets);
   if (isLoading) {
     return (
@@ -21,8 +23,19 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={()=> setShowModal(true) }>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+       {
+        showModal? (<Modal>
+          <div>
+        <h1>Would you like to adopt {pet.name}?</h1>
+        <div className="buttons">
+          <button>Yes</button>
+          <button onClick={() => setShowModal(false)}>No</button>
+        </div>
+      </div>
+        </Modal>) : null
+       }
       </div>
     </div>
   );
